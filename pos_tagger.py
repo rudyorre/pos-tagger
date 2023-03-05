99334e615a82db74b1e5d2591c1f60d618c12b6c
rudyorre <rudyorre@gmail.com>
2023-03-04 20:41:25 -0800

    Create pos tagger

--- pos_tagger.py
+++ pos_tagger.py
@@ -36,7 +36,6 @@ class POSTagger():
         self.tran_prob = self.compute_tran_prob()
         self.emis_prob = self.compute_emis_prob()
 
-
     def _get_ngrams(self, sent, n):
         """
         Given a text sentence and the argument n, we convert it to a list of n-grams.
@@ -47,7 +46,7 @@ class POSTagger():
             ngrams: a list of n-gram (tuples if n != 1, otherwise strings)
         """
         ngrams = []
-        for i in range(len(sent)-n+1):
+        for i in range(len(sent) - n + 1):
             ngram = tuple(sent[i:i+n]) if n != 1 else sent[i]
             ngrams.append(ngram)
         return ngrams
@@ -71,38 +70,50 @@ class POSTagger():
     def compute_tran_prob(self):
         """
         Compute the transition probability.
+
+        P(tagB | tagA) = P(tagA | tagB) * P(tagB) / P(tagA) = P(tagB, tagA) / P(tagA)
+
         Returns:
             tran_prob: a dictionary that maps each (tagA, tagB) tuple to its transition probability P(tagB|tagA).
         """
         tran_prob = defaultdict(lambda: 0) # if a tuple is unseen during training, we set its transition praobility to 0
         for tag_bigram in self.tag_bigram_cnt:
-            tran_prob[tag_bigram] = None # TODO: replace None
+            tagA, _ = tag_bigram
+            tran_prob[tag_bigram] = self.tag_bigram_cnt[tag_bigram] / self.tag_unigram_cnt[tagA]
         return tran_prob
 
     def compute_emis_prob(self):
         """
         Compute the emission probability.
+
+        P(word|tag) = P(tag|word)P(word)/P(tag) = P(tag & word) / P(tag)
+
         Returns:
             emis_prob: a dictionary that maps each (tagA, wordA) tuple to its emission probability P(wordA|tagA).
         """
-        emis_prob = defaultdict(lambda: 0) # if a tuple is unseen during training, we set its transition praobility to 0
+        emis_prob = defaultdict(lambda: 0) # if a tuple is unseen during training, we set its emission probability to 0
         for tag, word in self.tag_word_cnt:
-            emis_prob[(tag, word)] = None # TODO: replace None
+            emis_prob[(tag, word)] = self.tag_word_cnt[(tag, word)] / self.tag_unigram_cnt[tag]
         return emis_prob
 
     def init_prob(self, tag):
         """
         Compute the initial probability for a given tag.
+
+        P(tag|<bos>) = P(<bos>, tag) / P(<bos>)
+
         Returns:
-            tag_init_prob (float): the initial probaiblity for {tag}
+            tag_init_prob (float): the initial probability for {tag}
         """
-        tag_init_prob = None # TODO: replace None
-        return tag_init_prob
+        return self.tag_bigram_cnt['<bos>', tag] / self.tag_unigram_cnt['<bos>']
 
     def viterbi(self, sent):
         """
         Given the computed initial/transition/emission probability, make predictions for a given
         sentence using the Viterbi algorithm.
+
+        Implementation based off of https://en.wikipedia.org/wiki/Viterbi_algorithm
+
         Args:
             sent: a list of words (strings)
         Returns:
@@ -115,33 +126,43 @@ class POSTagger():
             Input: ['Mike', 'caught', 'the', 'ball', 'just', 'as', 'the', 'catcher', 'slid', 'into', 'the', 'bag', '.']
             Output: ['NP', 'VBD', 'AT', 'NN', 'RB', 'CS', 'AT', 'NN', 'VBD', 'IN', 'AT', 'NN', '.']
         """
-        
-        # TODO implement the Viberti algorithm for POS tagging.
-        # We provide an example implementation below with parts of the code removed, but feel free
-        # to write your own implementation.
-        """
-        V = {}
-        backtrack = {}
-        for step, word in enumerate(sent):
-            for tag in self.all_tags:
-                if step == 0:
-                    V[tag, step] = None #replace None
-                else:
-                    pass #replace pass
-                    for prev_tag in self.all_tags:
-                        pass #replace pass
-                    V[(tag, step)] = None #replace None
-                    backtrack[(tag, step)] = #replace None
+        trellis = {}
+        pointers = {}
 
-        prev_tag = None #replace None
+        # Find tag probabilities for first word
+        for tag in self.all_tags:
+            trellis[tag, 0] = self.init_prob(tag) * self.emis_prob[tag, sent[0]]
+        
+        # Find tag probabilities for rest of the words
+        for step in range(1, len(sent)):
+            word = sent[step]
+            for tag in self.all_tags:
+                max_tag, max_prob = 'NN', trellis['NN', step - 1] * self.tran_prob['NN', tag] * self.emis_prob[tag, word]
+                for prev_tag in self.all_tags:
+                    prob = trellis[prev_tag, step - 1] * self.tran_prob[prev_tag, tag] * self.emis_prob[tag, word]
+                    if prob > max_prob:
+                        max_tag = prev_tag
+                        max_prob = prob
+                trellis[tag, step] = max_prob
+                pointers[tag, step] = max_tag
+
+        # Find most probable final tag
+        last_step = len(sent) - 1
+        max_tag, max_prob = 'NN', 0.0
+        for tag, step in trellis:
+            if step == last_step and trellis[tag, step] > max_prob:
+                max_tag = tag
+                max_prob = trellis[tag, step]
+
+        # Backtrack to find most probable tags at each step
+        prev_tag = max_tag
         pos_tag = [prev_tag]        
         for step in range(len(sent)-1, 0, -1):
-            prev_tag = backtrack[(prev_tag, step)]
+            prev_tag = pointers[prev_tag, step]
             pos_tag.append(prev_tag)
         pos_tag = pos_tag[::-1]
-        """
+
         return pos_tag
-                        
 
     def test_acc(self, corpus, use_nltk=False):
         """
@@ -167,8 +188,6 @@ class POSTagger():
         acc = cor/tot
         return acc
         
-                    
-
 if __name__ == '__main__':
     # Parse arguments
     parser = argparse.ArgumentParser()
@@ -176,6 +195,7 @@ if __name__ == '__main__':
             help='Will print information that is helpful for debug if set to True. Passing the empty string in the command line to set it to False.')
     parser.add_argument('--use_nltk', type=bool, default=False,
             help='Whether to evaluate the nltk model. Need to install the package if set to True.')
+    parser.add_argument('--input', type=str, help='')
     args = parser.parse_args()
 
     random.seed(42)
@@ -203,5 +223,8 @@ if __name__ == '__main__':
 
     # Tags for custom sentence
     custom_sentence = "Eddie caught the ball .".split()
-    tags = None # TODO: Get model predicted tags for the custom sentence
-    print (tags)
+    tags = pos_tagger.viterbi(custom_sentence) # TODO: Get model predicted tags for the custom sentence
+    print(custom_sentence, tags)
+    if args.input:
+        custom_tags = pos_tagger.viterbi(args.input.split())
+        print(args.input.split(), custom_tags)
